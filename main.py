995b7132da46d6367568c9895d34afdec13db993
costa <costa@DESKTOP-BKU93S9>
2023-07-07 18:47:14 +0300

    Added logs with timestamp

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from utils import write_to_log, write_to_file, read_from_file
 
 
 
-write_to_log("log1.txt", "Hello")
+write_to_log("Hello")
--- utils.py
+++ utils.py
@@ -1,15 +1,17 @@
 import logging
 import os
+from datetime import datetime
+
 from settings import get_dirs, get_base_path
 
-logging.basicConfig(
-    filename='logs/status.log',
-    format='%(asctime)s %(levelname)-8s %(message)s',
-    level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S')
 
-
-def write_to_log(log_path, content):
+def write_to_log(content):
+    logging.basicConfig(
+        filename='logs/' + f'status_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}',
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S',
+        filemode='w')
     BASE_PATH = get_base_path()
     print(f"BASE PATH IS: {BASE_PATH}")
     DIRS = get_dirs(BASE_PATH)
